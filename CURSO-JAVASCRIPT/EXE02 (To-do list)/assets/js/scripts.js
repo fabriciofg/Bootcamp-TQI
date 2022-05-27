1b13ecddf95cef01b03ea2658e04c60ed16c8aab
Fabrício Fraga Gimenez <36505529+fabriciofg@users.noreply.github.com>
2022-05-27 11:41:21 -0300

    novo desafio

--- CURSO-JAVASCRIPT/EXE02 (To-do list)/assets/js/scripts.js
+++ CURSO-JAVASCRIPT/EXE02 (To-do list)/assets/js/scripts.js
@@ -1,53 +1,11 @@
-let currentNumberWrapper = document.getElementById('currentNumber');
-
-let butonSubtrair = document.getElementById('subtrair')
 let butonAdicionar = document.getElementById('adicionar')
-
-let currentNumber = 0;
-
+let inpultItem = document.getElementById('item')
 
 
-function increment(){
-    currentNumber = currentNumber +1;
-    currentNumberWrapper.innerHTML = currentNumber;
+let teste = `teste ${inpultItem}`
 
-    desabilitaNegativo()
- 
-    if(currentNumber > 0){
-        currentNumberWrapper.style.color = 'blue'
-        currentNumberWrapper.style.fontWeight = 'bold'
-    }else if(currentNumber == 0){
-        currentNumberWrapper.style.color = 'black'
-        currentNumberWrapper.style.fontWeight = 'normal'
-    }
-}
+function adicionar(){    
+    console.log ('habilitado')
+    console.log (teste)
 
-function decrement(){
-    currentNumber = currentNumber -1;
-    currentNumberWrapper.innerHTML = currentNumber;    
-
-    desabilitaNegativo()    
-
-    if(currentNumber < 0){
-        currentNumberWrapper.style.color = 'red'
-        currentNumberWrapper.style.fontWeight = 'bold'
-    }
-    else if(currentNumber == 0){
-        currentNumberWrapper.style.color = 'black'
-        currentNumberWrapper.style.fontWeight = 'normal'
-    }
-    
-}
-
-function desabilitaNegativo(){
-  
-    if(currentNumber <= -10){
-        console.log ('desabilitado')
-        butonSubtrair.disabled = true
-        return true
-    }else{
-        console.log ('habilitado')
-        butonSubtrair.disabled = false
-        return false       
-    }
 }
