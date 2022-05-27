1a1ef9321d7c334f22808c2318a1739ff0bc8f89
Fabrício Fraga Gimenez <36505529+fabriciofg@users.noreply.github.com>
2022-05-27 13:23:41 -0300

    reorganização

--- CURSO-JAVASCRIPT/EXE02 (To-do list)/assets/js/scripts.js
+++ CURSO-JAVASCRIPT/EXE02 (To-do list)/assets/js/scripts.js
@@ -2,13 +2,9 @@ let butonAdicionar = document.getElementById('adicionar')
 let inpultItem = document.getElementById('item')
 let listaItens = document.getElementById('listaItens')
 
-
-
-
-
 function adicionar(){     
     console.log (inpultItem.value)
-    listaItens.innerHTML += `<li><input type="checkbox" name="checkboxItem" id="${inpultItem.value}">${inpultItem.value} </li>`
+    listaItens.innerHTML += `<li type="none"><input type="checkbox" name="checkboxItem" id="${inpultItem.value}">${inpultItem.value} </li>`
     listaItens.addEventListener
 
 }
